6d50d6a92d12700da7e727072a06eb85c3c052ab
Yune <jiyune1997@gmail.com>
2026-02-01 17:37:33 +0100

    about done

--- my-homepage/src/sections/About.jsx
+++ my-homepage/src/sections/About.jsx
@@ -3,22 +3,25 @@ export default function About() {
   return (
     <section id="about" className="section">
       <h1>About</h1>
-      <h2>
+      <div className="TextBlock">
+        <h2>
         Business Information Systems graduate with experience in digital projects across 
-        corporate environments.
-      </h2>
-      <p>
+        corporate environments.<br/><br/>
+        </h2>
+        <p>
         I come from Business Information Systems and work at the intersection of technology 
         and application. As a master’s graduate, I bring experience from university projects 
         and student roles in corporate environments. My background combines technical 
         understanding, analytical thinking, and a strong focus on users, processes, and 
-        context. <br/>
+        context. <br/><br/>
         I enjoy working where structure, logic, and design come together, and where 
         different perspectives contribute to better solutions. I work in a focused and 
         reliable way and value clear communication and thoughtful collaboration. What 
         motivates me most is developing digital products and concepts that are clear, 
         maintainable, and genuinely useful.
-      </p>
+        </p>
+      </div>
+
     </section>
   );
 }
