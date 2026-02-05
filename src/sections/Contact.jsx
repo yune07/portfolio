3abc7f83f5ed62f7a35f04f5502091003003a720
jiyune <jiyune.whang@stud.tu-darmstadt.de>
2026-02-05 14:33:36 +0100

    bug fix download

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,39 +1,40 @@
 export default function Contact() {
     return (
         <section id="contact" className="section contact">
-  <div
-    className="scrollSentinel"
-    data-id="contact"
-    style={{ position: "absolute", top: 0 }}
-  />
+            <div
+                className="scrollSentinel"
+                data-id="contact"
+                style={{ position: "absolute", top: 0 }}
+            />
             <h1>Contact</h1>
             <div className="contactContent">
 
-            <p className="contactIntro">
-                If you would like to talk, collaborate, or simply exchange ideas, feel free
-                to reach out.
-            </p>
+                <p className="contactIntro">
+                    If you would like to talk, collaborate, or simply exchange ideas, feel free
+                    to reach out.
+                </p>
 
-            <div className="contactLinks">
-                <a href="mailto:jiyune1997@gmail.com">jiyune1997@gmail.com</a>
-                <a
-                    href="https://www.linkedin.com/in/ji-yune-whang-8397541b8/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    LinkedIn
-                </a>
-                                <a
-                    href="https://github.com/yune07"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    GitHub
-                </a>
-                <a href="/CV_JiYune.pdf" download>
-  Download CV
-</a>
-            </div>
+                <div className="contactLinks">
+                    <a href="mailto:jiyune1997@gmail.com">jiyune1997@gmail.com</a>
+                    <a
+                        href="https://www.linkedin.com/in/ji-yune-whang-8397541b8/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        LinkedIn
+                    </a>
+                    <a
+                        href="https://github.com/yune07"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        GitHub
+                    </a>
+                    <a href={`${import.meta.env.BASE_URL}CV_JiYune.pdf`} download>
+                        Download CV
+                    </a>
+
+                </div>
             </div>
         </section>
 
